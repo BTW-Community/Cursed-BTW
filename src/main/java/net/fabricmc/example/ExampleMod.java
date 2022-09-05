03abbbedde1680aab4770944c61b5db826071ffa
Arminias <arminias@t-online.de>
2022-09-05 17:32:03 +0200

    Fixed build cache, fixed example mixin
    Example mixin no longer gets applied to dedicated server, since it is not compatible

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,20 +1,22 @@
 package net.fabricmc.example;
 
-import com.prupe.mcpatcher.MCLogger;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.src.FCAddOnHandler;
+
+import java.util.logging.Logger;
 
 public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final MCLogger LOGGER = MCLogger.getLogger("modid");
+	public static final Logger LOGGER = Logger.getLogger("modid");
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		LOGGER.info("Hello Fabric world!");
+		FCAddOnHandler.LogMessage("Hello Fabric world!");
 	}
 
 }
